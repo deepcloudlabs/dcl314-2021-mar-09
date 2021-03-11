5a2957f1697a905e33a50dbdcb8106fe35711cac
deepcloudlabs <deepcloudlabs@gmail.com>
2021-03-11 14:22:34 +0300

    add express configuration

--- world-node-backend/world-service.js
+++ world-node-backend/world-service.js
@@ -1,3 +1,21 @@
+//region express configuration
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const port = 6400;
+const api = express();
+
+api.use(bodyParser.json({limit: "1mb"}));
+api.use(logger('dev'));
+api.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "HEAD, POST, PUT, DELETE, PATCH, GET");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+})
+//endregion
+
+//region mongo connection
 const mongoose = require("mongoose");
 const mongodb_url = "mongodb://localhost:27017/world";
 const mongodb_opts = {
@@ -7,9 +25,10 @@ const mongodb_opts = {
     "useCreateIndex": true,
     "useUnifiedTopology": true
 };
-
 mongoose.connect(mongodb_url, mongodb_opts);
+//endregion
 
+//region Schemas
 let CitySchema = new mongoose.Schema({
     "_id": Number,
     "name": String,
@@ -21,8 +40,8 @@ let CountrySchema = new mongoose.Schema({
         type: String,
         required: false,
         default: "Africa",
-        enum: ["Asia","Africa", "Europe", "Oceania", "Antarctica",
-        "North America", "South America"]
+        enum: ["Asia", "Africa", "Europe", "Oceania", "Antarctica",
+            "North America", "South America"]
     },
     "name": {
         type: String,
@@ -38,11 +57,12 @@ let CountrySchema = new mongoose.Schema({
     },
     "cities": [CitySchema]
 })
-
 let Country = mongoose.model('countries1', CountrySchema);
+//endregion
+
 
 Country.find({"continent": "Asia"},
-            (err,asianCountries) => {
-                   asianCountries.forEach( ctry => console.log(ctry.name));
-            }
+    (err, asianCountries) => {
+        asianCountries.forEach(ctry => console.log(ctry.name));
+    }
 );
